a7eae31b8e0be1c631a5e844183e5cd8fd5b647c
kluckmuck <kluckmucki@gmail.com>
2019-07-02 12:11:35 +0200

    frontend now uses hosted backend. changed looks of admin panel

--- jedjango/signup/admin.py
+++ jedjango/signup/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from .models import Signup
 
 # Register your models here.
-admin.site.register(Signup)
+# Register model in admin panel and make layout user friendly
+@admin.register(Signup)
+class SignupAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created'
+    search_fields = ['name', 'email', 'birthday']
+    list_display = ('name', 'email', 'birthday')
+
+# Admin site customization
+admin.site.site_header = 'Jacuzzi Entertainment Admin'
+admin.site.site_title = 'Jacuzzi Entertainment Admin'
+admin.site.index_title = ""
