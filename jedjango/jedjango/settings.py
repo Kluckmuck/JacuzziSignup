4fbc54759ccb3a8a936388b03e77a78c4d757162
Kluckmuck <kluckmucki@gmail.com>
2019-07-03 19:29:19 +0200

    fixed django secret for deploying. created super users

--- jedjango/jedjango/settings.py
+++ jedjango/jedjango/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'df=$ags+*z!p&bdl24fe&isa36g=5r&x+(awwcy)2-*02%(l&!'
 with open('jedjango/secret.txt') as f:
     SECRET_KEY = f.read().strip()
 
